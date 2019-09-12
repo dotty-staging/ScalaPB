95515e8d052a3b36f6fad168838e874420360de4
Liu Fengyun <liu@fengy.me>
2019-09-12 22:09:19 +0200

    Fix case object apply

--- scalapb-runtime/shared/src/main/scala/scalapb/textformat/ProtoAsciiParser.scala
+++ scalapb-runtime/shared/src/main/scala/scalapb/textformat/ProtoAsciiParser.scala
@@ -7,23 +7,23 @@ import scala.language.implicitConversions
 
 private[scalapb] object ProtoAsciiParser {
   def PrimitiveValue[$: P]: P[TPrimitive] = P(
-    (Index ~ Basics.fractional).map(TLiteral.tupled) |
-      (Index ~ Basics.bigInt).map(TIntLiteral.tupled) |
-      (Index ~ Basics.bytesLiteral).map(TBytes.tupled) |
-      (Index ~ Basics.literal).map(TLiteral.tupled)
+    (Index ~ Basics.fractional).map(TLiteral.apply.tupled) |
+      (Index ~ Basics.bigInt).map(TIntLiteral.apply.tupled) |
+      (Index ~ Basics.bytesLiteral).map(TBytes.apply.tupled) |
+      (Index ~ Basics.literal).map(TLiteral.apply.tupled)
   )
 
   def MessageValue[$: P]: P[TMessage] =
     P(
       Index ~ "{" ~/ KeyValue.rep ~/ "}" |
         Index ~ "<" ~/ KeyValue.rep ~/ ">"
-    ).map(TMessage.tupled)
+    ).map(TMessage.apply.tupled)
 
   def ValueArray[$: P]: P[TValue] =
-    P((Index ~ "[" ~/ (PrimitiveValue | MessageValue).rep(0, ",") ~/ "]")).map(TArray.tupled)
+    P((Index ~ "[" ~/ (PrimitiveValue | MessageValue).rep(0, ",") ~/ "]")).map(TArray.apply.tupled)
 
   def MessageArray[$: P]: P[TValue] =
-    P((Index ~ "[" ~/ MessageValue.rep(0, ",") ~/ "]")).map(TArray.tupled)
+    P((Index ~ "[" ~/ MessageValue.rep(0, ",") ~/ "]")).map(TArray.apply.tupled)
 
   def Value[$: P]: P[TValue] =
     P(
@@ -34,7 +34,7 @@ private[scalapb] object ProtoAsciiParser {
   def KeyValue[$: P]: P[TField] =
     P(
       Index ~ Basics.identifier ~/ Value
-    ).map(TField.tupled)
+    ).map(TField.apply.tupled)
 
-  def Message[$: P]: P[TMessage] = P(Index ~ KeyValue.rep ~ End).map(TMessage.tupled)
+  def Message[$: P]: P[TMessage] = P(Index ~ KeyValue.rep ~ End).map(TMessage.apply.tupled)
 }
